b0f8c8d0869d4e0afb31cf8728e5723d4e18095c
unknown <you@example.com>
2020-11-09 08:14:59 +0600

    1st commit done

---
+++ LAB_TASK_4.1_SESSION/publicHome.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>PUBLIC HOME</title>
+</head>
+<body>
+     <form>
+         <table border="2" width="60%">
+            <tr>
+               <td>
+                    <a href="public_home.php"><img src="gallery/logo.png" alt="Logo"></a>
+                </td>
+                <td align="right">
+                   <a href="public_home.php">Home</a>|
+                   <a href="login.php">Login</a>|
+                   <a href="registration.php">Registration</a>
+                </td>
+            </tr>
+            <tr>
+               <td colspan="2" height=200px><h2>Welcome to xCompany</h2></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><p>Copyright &#169; 2017</p></td>
+             </tr>
+        </table>   
+</body>
+</html>	
