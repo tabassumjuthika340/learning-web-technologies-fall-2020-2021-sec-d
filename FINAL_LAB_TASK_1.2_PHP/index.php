37352b218bfb1465fe471b2b60b668506090e761
unknown <you@example.com>
2020-12-02 08:41:10 +0600

    1st commit

---
+++ FINAL_LAB_TASK_1.2_PHP/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+    
+    <center>
+        
+        <br><br><br><br><br><br>
+        
+        <h1>Coupon Management System</h1>
+        
+        <form action="controller/loginCheck.php" method="post">
+        
+        <input type="text" name="username" placeholder="ID"><br><br>
+        <input type="password" name="password" placeholder="Password"><br><br>
+        <input type="submit" name="submit" value="Submit">
+        <a href="view/registration.php"><input type="button" name="signup" value="Sign Up"></a>
+        
+    </form>
+        
+    </center>
+    
+</body>
+</html>
