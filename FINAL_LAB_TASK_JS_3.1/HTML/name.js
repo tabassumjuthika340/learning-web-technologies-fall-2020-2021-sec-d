30afca1d52a5da4d38b5bbe7ab49beddc680d402
unknown <you@example.com>
2020-12-07 10:09:53 +0600

    name

---
+++ FINAL_LAB_TASK_JS_3.1/HTML/name.js
@@ -0,0 +1,10 @@
+function emailValidation(){
+    
+    let data = document.getElementById('name').value;
+    
+    if (data === "") {
+        
+        window.alert('Name is empty!');
+        
+    }
+    
