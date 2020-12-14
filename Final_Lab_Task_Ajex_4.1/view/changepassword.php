06fe601f4e8abbf693822b94490c68a395e988ff
unknown <you@example.com>
2020-12-14 10:14:14 +0600

    final

---
+++ Final_Lab_Task_Ajex_4.1/view/changepassword.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <title>Registration</title>
+</head>
+
+<body>
+
+
+    <table width="70%">
+            <form>
+         
+                        <fieldset>
+
+                            <legend>Registration</legend>
+
+                          
+                           
+							
+                            current Password : <input type="password" name=" current password"><br><br>
+							New Password : <input type="password" name="new Password"><br><br>
+							Retype New Password : <input type="password" name="retype new Password"><br><br>
+							
+                            
+                            <input type="submit" name="submit" value="Change"></a>
+							<a href="view/registration.php"><input type="button" name="sign" value="Home"></a>
+
+                        </fieldset>
+
+
+        </form>
+
+
+    </center>
+
+
+</body>
+
+</html>
+
